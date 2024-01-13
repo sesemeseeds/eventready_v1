b4e09810140b6ec226f435e11225e3e430d28de1
kbnguyen1423 <kbnguyen1423@gmail.com>
2024-01-13 15:42:07 -0500

    update changes

--- eventready/src/App.js
+++ eventready/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import GeneralInfoComponent from './components/GeneralInfo';
 
 function App() {
   return (
     <div>
-      <HelloWorld />
+      <GeneralInfoComponent />
     </div>
   );
 }
---
+++ eventready/src/components/GeneralInfo.js
@@ -0,0 +1,20 @@
+
+
+
+export default function GeneralInfoComponent() {
+    return (
+        <div>
+            <div>
+            <h1>Event Title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div>
+            <h1>  Side section</h1>
+            <div> asdfasdf asdf asdf asddasdf </div>
+            </div>
+        
+        
+      </div>
+    )
+
+}
