f556f48d16d8ca7d16465407242a77a86a0baa4a
RJoshi141 <ritikajoshi141@gmail.com>
2024-01-18 14:46:00 -0500

    marketing

--- eventready/src/pages/MarketingPage.js
+++ eventready/src/pages/MarketingPage.js
@@ -1,1 +1,71 @@
-
+import React, { useState } from 'react';
+import axios from 'axios';
+ 
+const ReminderApp = () => {
+  const [reminders, setReminders] = useState([]);
+  const [newReminder, setNewReminder] = useState({
+    name: '',
+    time: ''
+  });
+ 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewReminder({
+      ...newReminder,
+      [name]: value
+    });
+  };
+ 
+  const handleAddReminder = () => {
+    if (newReminder.name && newReminder.time) {
+      setReminders([...reminders, newReminder]);
+      setNewReminder({
+        name: '',
+        time: ''
+      });
+    }
+  };
+ 
+  return (
+    
+    <div className="reminder-app">
+      {/* Side Section */}
+      <div className="side-section">
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Reminder Name"
+            value={newReminder.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="time"
+            placeholder="Reminding Time"
+            value={newReminder.time}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button onClick={handleAddReminder}>+</button>
+        </div>
+      </div>
+ 
+      {/* Displaying Reminders */}
+      <div className="reminders">
+        <h2>Reminders</h2>
+        <ul>
+          {reminders.map((reminder, index) => (
+            <li key={index}>
+              <strong>{reminder.name}</strong> - {reminder.time}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default MarketingPage;
