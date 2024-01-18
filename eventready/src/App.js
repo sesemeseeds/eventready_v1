bc699f2717d145132c1b4475b1de8327a1c7de29
RJoshi141 <ritikajoshi141@gmail.com>
2024-01-18 14:45:13 -0500

    updating

--- eventready/src/App.js
+++ eventready/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { EventPage } from "./pages/EventPage";
 import GeneralInfoPage from "./pages/GeneralInfoPage";
+import MarketingPage from "./pages/MarketingPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/event" element={<EventPage />} />
         <Route path="/helloworld" element={<HelloWorld />} />
         <Route path="/generalinfo" element={<GeneralInfoPage />} />
+        <Route path="/marketing" element={<MarketingPage />} />
       </Routes>
     </>
   );
