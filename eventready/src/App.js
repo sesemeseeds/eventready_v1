44d3250cd7bd7ff62382adcdc396d04f53d8c0a3
karthikaankem09 <akarthika2001@gmail.com>
2024-01-19 16:23:56 -0500

    attendence

--- eventready/src/App.js
+++ eventready/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import HelloWorld from "./HelloWorld";
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { EventPage } from "./pages/EventPage";
 import GeneralInfoPage from "./pages/GeneralInfoPage";
+import Attendence from "./pages/Attendence";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/event" element={<EventPage />} />
         <Route path="/helloworld" element={<HelloWorld />} />
         <Route path="/generalinfo" element={<GeneralInfoPage />} />
+        <Route path="/Attendence" element={<Attendence />} />
       </Routes>
     </>
   );
