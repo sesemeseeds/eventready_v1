b2911bcbf447edb39b54d98640f9c8a7b1a61766
karthikaankem09 <akarthika2001@gmail.com>
2024-01-19 20:11:32 -0500

    attendence page

--- eventready/src/pages/Attendence.js
+++ eventready/src/pages/Attendence.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
 
-const EventPage = () => {
+const QRCodeGenerator = () => {
+    const [name, setName] = useState('');
+    const [userList, setUserList] = useState([]);
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        // Update the user list with the new name
+        setUserList((prevList) => [...prevList, name]);
+        // Clear the input field
+        setName('');
+    };
+
     return (
         <div>
-            <h1>Event Ready!</h1>
+            <h1>QR Code Generator</h1>
+
+            {/* QR Code for the external form */}
+            <QRCode value="URL_TO_EXTERNAL_FORM" />
+
+            {/* User Input Form */}
+            <form onSubmit={handleFormSubmit}>
+                <label>
+                    Enter your name:
+                    <input type="text" value={name} onChange={handleNameChange} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+
+            {/* User List Display */}
+            <h2>User List</h2>
+            <ul>
+                {userList.map((user, index) => (
+                    <li key={index}>{user}</li>
+                ))}
+            </ul>
         </div>
     );
 };
 
-export default EventPage;
+export default QRCodeGenerator;
+
 
 
