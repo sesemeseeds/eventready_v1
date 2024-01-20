45c4b3a68ef9b71fc0722e80309c4c7373e5514a
Emilio Sese <miovsese@gmail.com>
2024-01-20 13:46:23 -0500

    Project model creation

--- eventReadyBackend/eventReadyAPI/models.py
+++ eventReadyBackend/eventReadyAPI/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField("Name", unique=True, max_length=256)
+    doe = models.DateField("Day of Event")
+    start_time = models.TimeField("Start Time") 
+    end_time = models.TimeField("End Time")
+    location = models.CharField("Location", max_length=100)
+    description = models.CharField("Description", max_length=1024)
+    created = models.DateField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name    
