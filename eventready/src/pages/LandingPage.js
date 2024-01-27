3a8022123ce7adf11faab1cb936e9758cefcda1e
RJoshi141 <ritikajoshi141@gmail.com>
2024-01-27 03:39:23 -0500

    heading size

--- eventready/src/pages/LandingPage.js
+++ eventready/src/pages/LandingPage.js
@@ -1,5 +1,15 @@
+import React, { useState } from 'react';
+import { Button, TextField, Typography, Box, AppBar, Toolbar, Dialog, DialogTitle, DialogContent, DialogActions, Container } from '@mui/material';
+import QRCode from 'qrcode.react';
+
 export const LandingPage = () => {
     return (
-        <h1>Landing Page</h1>
+        <AppBar position="static" style={{ backgroundColor: 'red' }}>
+        <Toolbar>
+          <Typography variant="h5" style={{ color: 'white', fontWeight: 'bold' }}>
+            Landing Page
+          </Typography>
+        </Toolbar>
+      </AppBar>
     )
 }
