cb36469e9f09f2df890d82f76c6f2f7a8fff9566
RJoshi141 <ritikajoshi141@gmail.com>
2024-01-27 02:45:03 -0500

    mp

--- eventready/src/pages/MarketingPage.js
+++ eventready/src/pages/MarketingPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, TextField, Button, Box, Container } from '@mui/material';
+import { Button, TextField, Typography, Box, AppBar, Toolbar, Dialog, DialogTitle, DialogContent, DialogActions, Container } from '@mui/material';
+import QRCode from 'qrcode.react';
 
 const MarketingPage = () => {
   const [reminders, setReminders] = useState([]);
@@ -7,6 +8,7 @@ const MarketingPage = () => {
   const [reminderDateTime, setReminderDateTime] = useState('');
   const [image, setImage] = useState(null);
   const [caption, setCaption] = useState('');
+  const [shareDialogOpen, setShareDialogOpen] = useState(false);
 
   const addReminder = () => {
     if (reminderName && reminderDateTime) {
@@ -23,6 +25,26 @@ const MarketingPage = () => {
 
   const handleCaptionChange = (event) => {
     setCaption(event.target.value);
+  };
+
+  const openFacebook = () => {
+    window.open('https://www.facebook.com/');
+  };
+
+  const openInstagram = () => {
+    window.open('https://www.instagram.com/');
+  };
+
+  const openOutlook = () => {
+    window.open('https://outlook.live.com/');
+  };
+
+  const openShareDialog = () => {
+    setShareDialogOpen(true);
+  };
+
+  const closeShareDialog = () => {
+    setShareDialogOpen(false);
   };
 
   return (
@@ -58,13 +80,13 @@ const MarketingPage = () => {
               flex: 1,
               padding: '20px',
               marginRight: '20px',
-              backgroundColor: '#C1FFC1', // Minty green background
+              backgroundColor: '#e5ffd2',
               borderRadius: '10px',
             }}
           >
             <Typography
               variant="h6"
-              style={{ color: '#006400', fontWeight: 'bold', marginBottom: '10px' }} // Dark green color
+              style={{ color: '#006400', fontWeight: 'bold', marginBottom: '10px' }}
             >
               Reminders
             </Typography>
@@ -75,6 +97,7 @@ const MarketingPage = () => {
                 value={reminderName}
                 onChange={(e) => setReminderName(e.target.value)}
                 fullWidth
+                style={{ marginBottom: '10px' }}
               />
               <TextField
                 type="datetime-local"
@@ -82,10 +105,13 @@ const MarketingPage = () => {
                 onChange={(e) => setReminderDateTime(e.target.value)}
                 InputLabelProps={{ shrink: true }}
                 fullWidth
+                
               />
               <Button
-                style={{ backgroundColor: '#008B8B', color: '#FFFFFF', marginTop: '10px' }}
+                style={{ backgroundColor: '#009822', color: '#FFFFFF', marginTop: '15px' }}
+                variant="contained"
                 onClick={addReminder}
+                
               >
                 Add Reminder
               </Button>
@@ -103,13 +129,13 @@ const MarketingPage = () => {
             style={{
               flex: 1,
               padding: '20px',
-              marginRight: '20px', // Space between the second and third sections
-              backgroundColor: '#E6E6FA', // Lilac background
+              marginRight: '20px',
+              backgroundColor: '#f5e9ff',
               borderRadius: '10px',
             }}
           >
             <Typography variant="h6" style={{ color: '#8A2BE2', fontWeight: 'bold', marginBottom: '10px' }}>
-              Image Upload
+              Upload Graphic
             </Typography>
             <label htmlFor="file-upload" className="custom-file-upload" style={{ display: 'block', marginBottom: '10px' }}>
               Choose File
@@ -143,43 +169,82 @@ const MarketingPage = () => {
             style={{
               flex: 1,
               padding: '20px',
-              backgroundColor: '#FFE4B5', // Light Orange background
+              backgroundColor: '#ffe1d2',
               borderRadius: '10px',
             }}
           >
-            <Typography variant="h6" style={{ color: '#FF0000',fontWeight: 'bold', marginBottom: '10px' }}>
+            <Typography variant="h6" style={{ color: '#FF0000', fontWeight: 'bold', marginBottom: '10px' }}>
               Helpful Links
             </Typography>
+            <Box display="flex" justifyContent="space-between">
             <Button
-            variant="contained"
-            style={{ backgroundColor: '#FF0000', color: '#FFFFFF', marginRight: '10px' }} // Red color
-            onClick={() => window.open('https://www.canva.com/', '_blank')}
+              variant="contained"
+              style={{ backgroundColor: '#8F00FF', color: '#FFFFFF', marginRight: '10px', marginTop: '30px' }}
+              onClick={() => window.open('https://www.canva.com/', '_blank')}
             >
-            Canva
-          </Button>
-          <Button
-          variant="contained"
-          style={{ backgroundColor: '#FF0000', color: '#FFFFFF', marginRight: '10px' }} // Red color
-          onClick={() => window.open('https://www.figma.com/', '_blank')}
-          >
-      Figma
-    </Button>
-    <Button
-      variant="contained"
-      style={{ backgroundColor: '#FF0000', color: '#FFFFFF' }} // Red color
-      onClick={() => window.open('https://www.adobe.com/', '_blank')}
-    >
-      Adobe
-    </Button>
+              Canva
+            </Button>
+            <Button
+              variant="contained"
+              style={{ backgroundColor: '#0acf83', color: '#FFFFFF', marginRight: '10px', marginTop: '30px' }}
+              onClick={() => window.open('https://www.figma.com/', '_blank')}
+            >
+              Figma
+            </Button>
+            <Button
+              variant="contained"
+              style={{ backgroundColor: '#FF0000', color: '#FFFFFF', marginTop: '30px' }}
+              onClick={() => window.open('https://www.adobe.com/', '_blank')}
+            >
+              Adobe
+            </Button>
+            </Box>
             <div style={{ marginTop: '20px' }}>
-              <Typography style={{ fontSize: '14px', color: '#6A5ACD' }}>Share</Typography>
-              {/* Add content for sharing */}
+              <Button
+                variant="contained"
+                style={{ backgroundColor: '#0000FF', color: '#FFFFFF', marginTop: '50px' }}
+                onClick={openShareDialog}
+              >
+                Share Graphic
+              </Button>
             </div>
           </Box>
         </Box>
       </Container>
+
+      {/* Share Dialog */}
+      <Dialog open={shareDialogOpen} onClose={closeShareDialog}>
+        <DialogTitle>Share Options</DialogTitle>
+        <DialogContent>
+          <Box display="flex" flexDirection="column">
+            <Button
+              variant="contained"
+              style={{ marginTop: '10px', backgroundColor: '#316FF6.', color: '#FFFFFF' }}
+              onClick={openFacebook}
+            >
+              Facebook
+            </Button>
+            <Button
+              variant="contained"
+              style={{ marginTop: '10px', backgroundColor: '#d62976', color: '#FFFFFF' }}
+              onClick={openInstagram}
+            >
+              Instagram
+            </Button>
+            <Button
+              variant="contained"
+              style={{ marginTop: '10px', backgroundColor: '#0072c6', color: '#FFFFFF' }}
+              onClick={openOutlook}
+            >
+              Outlook
+            </Button>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeShareDialog}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
-
 export default MarketingPage;
