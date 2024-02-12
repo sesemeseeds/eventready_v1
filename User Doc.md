7b2330c6cffbee1b4a2fcacfdb60a484fcbac13d
Emilio Sese <miovsese@gmail.com>
2024-02-12 11:20:31 -0500

    merge from main

---
+++ User Doc.md
@@ -0,0 +1,90 @@
+# Project Setup Prerequisites
+
+1. Install Python3 ([https://www.python.org/downloads/](https://www.python.org/downloads/)) and latest version of Node.js ([https://nodejs.org/en](https://nodejs.org/en))
+2. Open a terminal within VS Code and use this line to clone repository.
+   ```bash
+   git clone https://github.com/kbnguyen1423/Capstone-Project.git
+   ```
+3. In the terminal run the following commands to set up necessary plug-ins:
+   ```bash
+   py -m install Django
+   pip install djangorestframework django-cors-headers
+   ```
+4. Open the root project folder **Capstone-Project**
+
+## Starting the Project Backend
+
+1. In a new terminal run the following:
+   ```bash
+   cd .\eventReadyBackend\  # to move to the backend folder
+   py manage.py runserver   # to start the backend server
+   ```
+2. Leave the terminal open while running the project
+
+## Starting the Project Frontend
+
+1. In a new terminal run the following
+   ```bash
+   cd .\eventready\  # to move to the frontend folder
+   npm install        # to install all required packages
+   npm start          # to start the frontend server
+   ```
+2. Web app will be opened on [http://localhost:3000/](http://localhost:3000/) and ready to be used.
+
+# Using the Project
+
+
+## User Authentication (WIP)
+
+Will be added in the future
+
+## Landing Page (WIP)
+
+1. Main page where events are displayed and created. (WIP) ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/9bb3cb23-a201-4c79-984c-28dd9d5d7527)
+
+2. Click Add Event Button to create event (WIP)
+      1. Fill out information listed. After submission you will be redirected to the event page. ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/e43a124a-2d81-4a70-8309-db0cbeeb100a)
+
+## Event Page – Navigation Bar
+
+1. Use this sidebar to navigate between all the pages listed
+   1. ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/7aaef085-ed32-408f-99bf-2a9eb7565c5f)
+
+## Event Page – General Info
+
+1. Event Basic Info is displayed on this page. ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/ac15d45c-7fd7-4f3d-a582-80bc2e0e05b8)
+
+2. Click on edit button to make changes to information on the page
+    1. On submission the information on the page will be updated ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/4e35516a-2ddc-4035-9317-64a258a176be)
+
+
+## Event Page – Goals
+
+Will be added in the future
+
+## Event Page – Tasks
+
+Will be added in the future
+
+## Event Page – Budget
+
+Will be added in the future
+
+## Event Page – Marketing (WIP)
+
+1. A repository where you can add reminders and save photos for your event ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/eee6aa62-54ac-4ff5-8383-541293534970)
+
+
+## Event Page – Attendance (WIP)
+
+1. A QR Code that can be scanned to fill out attendance list. ![image](https://github.com/kbnguyen1423/Capstone-Project/assets/98423738/60f6852c-937c-4851-9380-3e95901a293f)
+
+
+## FAQ
+
+1. **Q: How do I install Python and Node.js for the project?**
+   - **A:** Follow the installation instructions provided on their official websites: [Python](https://www.python.org/downloads/) and [Node.js](https://nodejs.org/en).
+
+2. **Q: What is the purpose of cloning the repository?**
+   - **A:** Cloning the repository is essential to get the project's source code on your local machine. It provides you with the necessary files to run and modify the project.
+
--- eventready/src/pages/MarketingPage.js
+++ eventready/src/pages/MarketingPage.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Button, TextField, Typography, Box, AppBar, Toolbar, Dialog, DialogTitle, DialogContent, DialogActions, Container } from '@mui/material';
+import canvaLogo from '../images/canva-logo.png';
+import figmaLogo from '../images/figma-logo.png';
+import adobeLogo from '../images/adobe-logo.png';
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 import AxiosInstance from "../components/Axios";
@@ -129,7 +132,7 @@ const MarketingPage = () => {
   const toggleUploadPoster = () => {
     setShowUploadPoster(!showUploadPoster);
   };
-  
+
   const toggleHelpfulLinks = () => {
     setShowHelpfulLinks(!showHelpfulLinks);
   };
@@ -137,7 +140,6 @@ const MarketingPage = () => {
   const toggleUploadRecapImages = () => {
     setShowUploadRecapImages(!showUploadRecapImages);
   };
-
 
   const handleImageUpload = (e) => {
     const selectedImage = e.target.files[0];
@@ -171,21 +173,30 @@ const MarketingPage = () => {
   };
   const openInstagram = () => {
     window.open('https://www.instagram.com/');
-  };
+  };  
   const openOutlook = () => {
     window.open('https://outlook.live.com/');
   };
+  
+  // const openShareDialog = () => {
+  //   setShareDialogOpen(true);
+  // };
+  
+  // const closeShareDialog = () => {
+  //   setShareDialogOpen(false);
+  // };
 
-  const handleRecapImageUpload = (e) => {
-    const selectedImages = e.target.files;
+  const handleRecapImageUpload = (event) => {
+    const selectedImages = event.target.files;
     setRecapImages([...recapImages, ...Array.from(selectedImages)]);
   };
-
+  
   const handleRecapImageClear = (index) => {
     const updatedImages = [...recapImages];
     updatedImages.splice(index, 1);
     setRecapImages(updatedImages);
   };
+
 
   return (
     <div>
@@ -216,11 +227,13 @@ const MarketingPage = () => {
           }}
         >
 
+
           {/* Reminders Section */}
           <Box
             style={{
               flex: 1,
               marginRight: '20px',
+              marginLeft: '20px',
               marginTop: '40px',
               marginBottom: '70px',
               
@@ -298,7 +311,6 @@ const MarketingPage = () => {
               )}
             </Box>
           </Box>
-
 
           {/* Upload Graphic Section */}
 
@@ -399,7 +411,9 @@ const MarketingPage = () => {
             style={{
               flex: 1,
               marginRight: '20px',
+              marginRight: '20px',
               marginTop: '40px',
+              marginBottom: '70px',
               marginBottom: '70px',
             }}
           >
@@ -429,43 +443,58 @@ const MarketingPage = () => {
 
               {/* Light yellow content */}
               {showHelpfulLinks && (
-                <Box
-                  style={{
-                    padding: '20px',
-                    marginTop: '-10px',
-                    backgroundColor: '#ffeec5',
-                    borderRadius: '5px 5px 10px 10px',
-                    display: showHelpfulLinks ? 'block' : 'none',
-                  }}
-                >
-                  <Typography variant="body1" style={{ color: 'black', marginBottom: '10px', marginTop: '5px' }}>
-                    Embarking on a creative journey with graphics? Dive into these fantastic resources to unleash your inner designer and craft amazing posters, vibrant flyers, eye-catching logos, and more for your spectacular event! Let your imagination run wild and bring your ideas to life!
-                  </Typography>
-                  <Box display="flex" justifyContent="space-between">
-                    <Button
-                      variant="contained"
-                      style={{ backgroundColor: '#20C4CB', color: '#FFFFFF', marginRight: '10px', marginTop: '10px' }}
-                      onClick={() => window.open('https://www.canva.com/', '_blank')}
-                    >
-                      Canva
-                    </Button>
-                    <Button
-                      variant="contained"
-                      style={{ backgroundColor: '#a259ff', color: '#FFFFFF', marginRight: '10px', marginTop: '10px' }}
-                      onClick={() => window.open('https://www.figma.com/', '_blank')}
-                    >
-                      Figma
-                    </Button>
-                    <Button
-                      variant="contained"
-                      style={{ backgroundColor: '#FF0000', color: '#FFFFFF', marginTop: '10px' }}
-                      onClick={() => window.open('https://www.adobe.com/', '_blank')}
-                    >
-                      Adobe
-                    </Button>
-                  </Box>
+              <Box
+                style={{
+                  padding: '20px',
+                  marginTop: '-10px',
+                  backgroundColor: '#ffeec5',
+                  borderRadius: '5px 5px 10px 10px',
+                  display: showHelpfulLinks ? 'block' : 'none',
+                }}
+              >
+                <Typography variant="body1" style={{ color: 'black', marginBottom: '10px', marginTop: '5px' }}>
+                  Embarking on a creative journey with graphics? Dive into these fantastic resources to unleash your inner designer and craft amazing posters, vibrant flyers, eye-catching logos, and more for your spectacular event! Let your imagination run wild and bring your ideas to life!
+                </Typography>
+                <Box display="flex" justifyContent="space-between">
+                  <Button
+                    style={{
+                      backgroundImage: `url(${canvaLogo})`,
+                      backgroundSize: 'cover',
+                      backgroundRepeat: 'no-repeat',
+                      width: '40px',
+                      height: '64px',
+                    }}
+                    onClick={() => window.open('https://www.canva.com/', '_blank')}
+                  >
+                  </Button>
+
+                  <Button
+                    style={{
+                      backgroundImage: `url(${figmaLogo})`,
+                      backgroundSize: 'cover',
+                      backgroundRepeat: 'no-repeat',
+                      width: '20px',
+                      height: '96px',
+                    }}
+                    onClick={() => window.open('https://www.figma.com/', '_blank')}
+                  >
+                  </Button>
+
+                  <Button
+                    style={{
+                      backgroundImage: `url(${adobeLogo})`,
+                      backgroundSize: 'cover',
+                      backgroundRepeat: 'no-repeat',
+                      width: '10px',
+                      height: '62px',
+                    }}
+                    onClick={() => window.open('https://www.adobe.com/', '_blank')}
+                  >
+                  </Button>
                 </Box>
-              )}
+              </Box>
+            )}
+
             </Box>
           </Box>
         </Box>
@@ -517,7 +546,6 @@ const MarketingPage = () => {
           <Typography variant="body1" style={{ color: 'black', marginTop: '55px' }}>
             Relive the magic of your events by uploading recap images. Let the pictures tell the story and showcase the vibrant essence of your organization.
           </Typography>
-
           
           {/* File input and label */}
           <label htmlFor="recap-image-upload" className="custom-file-upload">
@@ -550,13 +578,12 @@ const MarketingPage = () => {
                   onClick={() => handleRecapImageClear(index)}>
                   X
                 </Button>
-
               </div>
-            ))}
+              ))}
           </div>
         </Box>
         )}
-        </Box>
+      </Box>
       </Container>
     </div>
   );
