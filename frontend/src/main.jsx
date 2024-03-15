00a8b74aab9d794e9a2f365b0f42fbc2a162abae
Haneesha <haneesha207@gmail.com>
2024-03-15 19:50:10 -0400

    user-auth-changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
--- frontend/src/pages/UserLoginPage.jsx
+++ frontend/src/pages/UserLoginPage.jsx
@@ -1,59 +1,29 @@
 import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/clerk-react";
-import { Box, Typography, Paper, Button } from "@mui/material";
-import "../styles/UserLoginPage.css";
 
-const UserLoginPage = ({ handleLogin }) => {
-  const { isSignedIn, user, isLoaded, signIn } = useUser();
-
-  const handleSignIn = async () => {
-    try {
-      await signIn();
-      handleLogin(); // Call the handleLogin function passed as a prop
-    } catch (error) {
-      console.error("Error signing in:", error);
-      // Handle sign-in error
-    }
-  };
+const UserLoginPage = () => {
+  const { isSignedIn, user, isLoaded } = useUser();
 
   if (!isLoaded) {
     // Handle loading state however you like
-    return (
-      <div className="centered">
-        <Paper elevation={3} className="login-card">
-          <Typography variant="h5" align="center" gutterBottom>
-            Loading...
-          </Typography>
-        </Paper>
-      </div>
-    );
+    return <div>Loading...</div>;
   }
 
   return (
-    <div className="centered-container"> {/* Modified the class name */}
-      <Paper elevation={3} className="login-card">
-        <Typography variant="h4" align="center" gutterBottom>
-          Event Ready
-        </Typography>
-        <Box mt={4} display="flex" flexDirection="column" alignItems="center">
-          {isSignedIn ? (
-            <>
-              <UserButton />
-              <Typography variant="h6" mt={2}>
-                Hello, {user.fullName}!
-              </Typography>
-            </>
-          ) : (
-            <>
-              <Typography variant="body1" gutterBottom>
-                Please sign in
-              </Typography>
-              <Button variant="contained" color="primary" onClick={handleSignIn} sx={{ mt: 2 }}>
-                Sign In
-              </Button>
-            </>
-          )}
-        </Box>
-      </Paper>
+    <div>
+      <header>
+        <h1>User Login Page</h1>
+        {isSignedIn ? (
+          <div>
+            <UserButton />
+            <p>Hello {user.fullName}!</p>
+          </div>
+        ) : (
+          <div>
+            <SignInButton />
+            <p>Not signed in</p>
+          </div>
+        )}
+      </header>
     </div>
   );
 };
