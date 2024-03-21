f09959b6289571d8575ab679bb350b7ef3dc167c
karthikaankem09 <akarthika2001@gmail.com>
2024-03-21 19:05:55 -0400

    import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import TasksPage from "./pages/TasksPage";
 import UserLoginPage from "./pages/UserLoginPage";
 import BudgetPage from "./pages/BudgetPage";
 import GoalsPage from "./pages/GoalsPage";
+import ImportExcel from "./pages/ImportExcel";
 
 function App() {
   return (
@@ -23,11 +24,15 @@ function App() {
           <Route path="goals" element={<GoalsPage />} />
           <Route path="budget" element={<BudgetPage />} />
           <Route path="userlogin" element={<UserLoginPage />} />
+          <Route path="/Attendance" element={<Attendance />} />
+
         </Route>
         
         <Route path="/" element={<UserLoginPage />} />
         <Route path="/dashboard" element={<LandingPage />} />
       </Routes>
+      <ImportExcel />
+
       </Router>
   );
 }
