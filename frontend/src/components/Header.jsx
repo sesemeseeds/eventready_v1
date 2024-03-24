fcd4d5324c3620efce5d3cd84e2d881a0cf63623
Haneesha <haneesha207@gmail.com>
2024-03-24 13:50:40 -0400

    UserButton to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
+import { UserButton, useUser } from "@clerk/clerk-react";
 
 export default function Header() {
+  const { isSignedIn, user, isLoaded } = useUser();
+
   return (
     <div className="header">
-     <h1> <Link className="headerText" to="/">Event Ready!</Link></h1> 
+      <div className="empty-div"></div> {/* Empty div */}
+      <div className="logo-container">
+        <img src="../src/images/ERlogo.png" alt="Event Ready Logo" className="logo" />
+      </div>
+      <h1 className="headerText">
+        <Link to="/dashboard">Event Ready</Link>
+      </h1>
+      <div className="user-info">
+        {isLoaded && isSignedIn ? (
+          <div>
+            <UserButton />
+          </div>
+        ) : (
+          <p>Not signed in</p>
+        )}
+      </div>
+      <div className="empty-div"></div> {/* Empty div */}
     </div>
   );
 }
